7e3c81be8b360255252050dab5a5d82e431982c6
chenzeyu <czy3843@gmail.com>
2026-03-06 16:31:48 +0800

    添加知识库图片测试接口

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ router = APIRouter(prefix="/api")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -19,6 +19,12 @@ app.add_middleware(
 def health():
     return {"status": "ok"}
 
+@router.post("/image")
+def health():
+    # https://picsum.photos/200/200
+    # return {"url": "https://fastly.picsum.photos/id/477/1920/1080.jpg?hmac=8V4fJHSP89AfrwJxObukCzN7UvUriZ6QbXAC6lL-zDs"}
+    return {"url": "https://fastly.picsum.photos/id/867/200/200.jpg?hmac=o_T4KIW6jPbGySRv8Em8TaP9PH_tgegfmPaYJJ394Y4"}
+    return {"url": "https://fastly.picsum.photos/id/118/1920/1080.jpg?hmac=d42KrCka0ICF4up1cFKNNipUmKDdTx_tqasvhTEmGvM"}
 
 @router.get("/db")
 def db_health():
